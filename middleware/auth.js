9b7ac02f102c0e146b32b59d733b2e639228840b
Sukhman1609 <128071797+Sukhman1609@users.noreply.github.com>
2023-11-10 18:07:48 +0530

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+// const auth=(req,res,next)=>{
+//     console.log('auth middleware');
+//    next();  
+// }
+// const auth2=(req,res,next)=>{
+//     console.log('auth middleware2 is running this is second one')
+//    next();
+   
+   
+// }
+// module.exports={auth,auth2};
+const jwt=require("jsonwebtoken")
+const secret_key ='hello16';
+
+const auth=(req,res,next)=>{
+    const BearerToken=req.headers["authorization"]
+   
+    if(BearerToken){
+        const token=BearerToken.split(" ")[1];
+
+        const validate=jwt.verify(token,secret_key);
+        if(validate){
+            next();
+        }
+        return res.send({msg:"User is not Authorized"})
+    }
+    return res.send({msg:"User is not Allowed...Please try again"})
+}
+module.exports=auth;
